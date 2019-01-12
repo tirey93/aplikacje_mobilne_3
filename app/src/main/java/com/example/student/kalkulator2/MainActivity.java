b852cf38852d385bac4202561eb92163620aa0d4
tirey93 <tirey93@gmail.com>
2019-01-12 13:37:07 +0100

    obsluga obracania ekranu

--- app/src/main/java/com/example/student/kalkulator2/MainActivity.java
+++ app/src/main/java/com/example/student/kalkulator2/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
+
         super.onConfigurationChanged(newConfig);
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
